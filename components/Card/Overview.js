db26ea4f3e88550e67df070ad49618c2d1bc9a9a
Brayan <brayan.loucher@outlook.com>
2022-06-28 22:41:05 +0200

    change font size for the overview aside

--- components/Card/Overview.js
+++ components/Card/Overview.js
@@ -40,7 +40,7 @@ const Overview = ({
                   <span className="text-[8px] lg:text-[11px] tracking-[0.73px] lg:tracking-[1px] text-white/50 uppercase">
                       Rotation Time
                   </span>
-                  <span className="text-[20px] lg:text-[40px] font-antonio-regular">
+                  <span className="text-[20px] lg:text-[30px] xl:text-[40px] font-antonio-regular">
                       {rotationTime}
                   </span>
                 </div>
@@ -48,7 +48,7 @@ const Overview = ({
                   <span className="text-[8px] lg:text-[11px] tracking-[0.73px] lg:tracking-[1px] text-white/50 uppercase">
                       Revolution Time
                   </span>
-                  <span className="text-[20px] lg:text-[40px] font-antonio-regular">
+                  <span className="text-[20px] lg:text-[30px] xl:text-[40px] font-antonio-regular">
                       {revolutionTime}
                   </span>
                 </div>
@@ -56,7 +56,7 @@ const Overview = ({
                   <span className="text-[8px] lg:text-[11px] tracking-[0.73px] lg:tracking-[1px] text-white/50 uppercase">
                       Radius
                   </span>
-                  <span className="text-[20px] lg:text-[40px] font-antonio-regular">
+                  <span className="text-[20px] lg:text-[30px] xl:text-[40px] font-antonio-regular">
                       {radius}
                   </span>
                 </div>
@@ -64,7 +64,7 @@ const Overview = ({
                   <span className="text-[8px] lg:text-[11px] tracking-[0.73px] lg:tracking-[1px] text-white/50 uppercase">
                       Average Temp.
                   </span>
-                  <span className="text-[20px] lg:text-[40px] font-antonio-regular">
+                  <span className="text-[20px] lg:text-[30px] xl:text-[40px] font-antonio-regular">
                       {averageTemp}
                   </span>
                 </div>
